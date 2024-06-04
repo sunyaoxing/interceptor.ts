7f0c0f0099bfbdc61ec6be4f00a03d8b69640658
Aleck Sun <aleck.sun@gmail.com>
2024-06-04 17:05:48 +0800

    update packaging

---
+++ bin/build-package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BASEDIR=$(dirname $0)
+PACKAGE_DIR=$1
+echo $BASEDIR $PACKAGE_DIR
+
+mkdir $PACKAGE_DIR
+cp -r $BASEDIR/../dist/* $PACKAGE_DIR/ 
+cp $BASEDIR/../package.json $PACKAGE_DIR/
